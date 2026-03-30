0d830c8b0a72d62013bf498afe14876442dcb14c
rakoon19 <rahatakondo18@gmail.com>
2026-03-30 19:54:33 +0600

    added testimonial

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
+import Testimonial from './Components/Testimonial/Testimonial'
 
 
 function App() {
@@ -11,6 +12,14 @@ function App() {
     <Navbar></Navbar>
     <Hero></Hero>
     <About></About>
+    {/* <Feature></Feature>
+    <FeatureCard></FeatureCard>
+    <Step></Step>
+    <StepCard></StepCard>
+    <Pricing></Pricing>
+    <PricingCard></PricingCard> */}
+    <Testimonial></Testimonial>
+    {/* <Footer></Footer> */}
     </>
   )
 }
---
+++ src/Components/Testimonial/Testimonial.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <div className='flex flex-col gap-10 py-30 justify-center items-center text-center'>
+
+            <div className='flex flex-col gap-4'>
+                <h1 className='text-4xl'>Ready to Transform Your Workflow?</h1>
+                <p>Join thousands of professionals who are already using Digitools to work smarter.Start your free trial today.</p>
+            </div>
+
+            <div className='flex flex-col gap-4'>
+                <div className='flex gap-4 justify-center'>
+                    <button>Explore Products</button>
+                    <button>View Pricing</button>
+                </div>
+                <p>14-day free trial • No credit card required • Cancel anytime</p>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonial;
