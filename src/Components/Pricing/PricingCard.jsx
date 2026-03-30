cce8fc68968dbdd3e4cd56ebb3bd48ff063fc3bd
rakoon19 <rahatakondo18@gmail.com>
2026-03-30 20:53:47 +0600

    lil refinement

--- src/Components/Pricing/PricingCard.jsx
+++ src/Components/Pricing/PricingCard.jsx
@@ -5,7 +5,7 @@ const PricingCard = ({ card }) => {
   <div key={card.id} className="">
     <h2>{card.name}</h2>
     <p>{card.description}</p>
-    <h1>${card.price}/{card.billing}</h1>
+    <p>${card.price}/{card.billing}</p>
 
     {card.popular && <span>{card.badge}</span>}
 
