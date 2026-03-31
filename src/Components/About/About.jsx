113d744e011cecd98ee53b454c2c6f30191bd234
rakoon19 <rahatakondo18@gmail.com>
2026-03-31 11:06:42 +0600

    designed about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,8 +5,16 @@ import AboutItems from './AboutItems';
 const About = () => {
 
     return (
-        <div className='flex justify-around'>
-        { about.map((item, index) => (<AboutItems statName={ item.statName } statNum={ item.statNum }key= {index}></AboutItems>))}
+        <div className='flex justify-around items-center bg-linear-to-r from-[#6366F1] via-[#8B5CF6] to-[#D946EF] text-white py-15'>
+            {about.map((item, index) => (
+                <React.Fragment key={index}>
+                    <AboutItems statName={item.statName} statNum={item.statNum} />
+
+                    {index !== about.length - 1 && (
+                        <div className="divider lg:divider-horizontal before:bg-white/20 after:bg-white/20"></div>
+                    )}
+                </React.Fragment>
+            ))}
         </div>
     );
 };
--- src/Components/About/AboutItems.jsx
+++ src/Components/About/AboutItems.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const AboutItems = ({ statNum, statName}) => {
     return (
+        <>
         <div className='flex flex-col gap-3'>
-            <h2 className='text-15'>{ statNum }</h2>
-            <p className='text-6'>{ statName}</p>
+            <h2 className='text-6xl font-extrabold'>{ statNum }</h2>
+            <p className='text-2xl font font-medium'>{ statName}</p>
         </div>
+        </>
     );
 };
 
