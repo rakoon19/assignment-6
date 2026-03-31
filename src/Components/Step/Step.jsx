cc3acc4908204fbe2983a97c7fb8c71f22d1e162
rakoon19 <rahatakondo18@gmail.com>
2026-03-31 11:52:06 +0600

    designed step

--- src/Components/Step/Step.jsx
+++ src/Components/Step/Step.jsx
@@ -4,12 +4,12 @@ import StepCard from './StepCard';
 const Step = () => {
     return (
         <div className='flex flex-col gap-10 py-30'> 
-            <div>
-                <h1 className='text-5xl'>Get Started in 3 Steps</h1>
-                <p>Start using premium digital tools in minutes, not hours.</p>
+            <div className='text-center flex flex-col gap-4'>
+                <h1 className='text-5xl font-extrabold'>Get Started in 3 Steps</h1>
+                <p className='text-[#627382]'>Start using premium digital tools in minutes, not hours.</p>
             </div>
 
-            <div className='flex gap-7.5 justify-between'>
+            <div className='flex gap-7.5 justify-center'>
                 { step.map((card) => ( <StepCard card={ card } key={ card.serialNo }></StepCard>))}
             </div>
 
--- src/Components/Step/StepCard.jsx
+++ src/Components/Step/StepCard.jsx
@@ -1,12 +1,22 @@
-
-
 const StepCard = ({ card }) => {
-    return (
-        <div className='flex flex-col item-center justify-center text-center'>
-            <span className="text-right">{ card.serialNo }</span>
-            <img src={ card.imgPath} alt="" className='w-25 self-center'/>
-            <h3 className="m-4 text-2xl">{ card.heading }</h3>
-            <p>{ card.para }</p>
+return (
+        <div className='relative flex flex-col items-center justify-center text-center p-10 w-full max-w-95 bg-white border border-gray-100 rounded-3xl shadow-sm hover:shadow-md transition-shadow '>
+
+            <span className="absolute top-6 right-6 flex items-center justify-center text-[14px] w-8 h-8 rounded-full bg-linear-to-r from-[#7C3AED] to-[#A855F7] text-white font-bold shrink-0">
+                { card.serialNo }
+            </span>
+
+            <div className='w-24 h-24 rounded-full bg-[#F3E8FF] flex items-center justify-center mb-6'>
+                <img src={ card.imgPath } alt={ card.heading } className='w-12 h-12 object-contain'/>
+            </div>
+
+            <h3 className="text-2xl font-bold text-[#101727] mb-3">
+                { card.heading }
+            </h3>
+            
+            <p className='text-[#627382] leading-relaxed'>
+                { card.para }
+            </p>
         </div>
     );
 };
